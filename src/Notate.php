d385c22c66e5030101683f63bc43add33c8f6783
Zac Olden <zacolden@gmail.com>
2018-06-15 16:43:10 +1000

    Partial JSON conversion support
    For now it assumes a class given will be based on Collection/Jsonable

--- src/Notate.php
+++ src/Notate.php
@@ -2,6 +2,9 @@
 
 namespace Notate;
 
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Notate\Relations\{HasOne,HasMany,BelongsTo};
 
@@ -66,5 +69,98 @@ trait Notate
         $ownerKey = $ownerKey ?? $instance->getKeyName();
         $relation = $relation ?? $this->guessBelongsToRelation();
         return new BelongsTo($instance->newQuery(), $this, $foreignKey, $ownerKey, $relation);
+    }
+
+    /**
+     * Create a new model instance that is existing.
+     *
+     * @param array $attributes
+     * @param null $connection
+     * @return mixed
+     */
+    public function newFromBuilder($attributes = [], $connection = null)
+    {
+        $model = parent::newFromBuilder($attributes, $connection);
+        $model->convertJson();
+        return $model;
+    }
+
+    /**
+     * Perform a model update operation.
+     *
+     * @param Builder $query
+     * @return bool
+     */
+    protected function performUpdate(Builder $query)
+    {
+        $this->convertJson();
+        parent::performUpdate($query);
+        $this->convertJson();
+        return true;
+    }
+
+    /**
+     * Perform a model insert operation.
+     *
+     * @param Builder $query
+     * @return bool
+     */
+    protected function performInsert(Builder $query)
+    {
+        $this->convertJson();
+        parent::performInsert($query);
+        $this->convertJson();
+        return true;
+    }
+
+    /**
+     * Convert a model's JSON columns into a specified data type.
+     *
+     * @param Model|null $model
+     * @return Model|Notate
+     */
+    protected function convertJson(Model $model = null)
+    {
+        if(!$model) { $model = $this; }
+        $class = Config::getClass();
+        if($class)
+        {
+            foreach($model->attributes as $key => $value)
+            {
+                if(is_string($value))
+                {
+                    if($this->isJson($value))
+                    {
+                        $this->{$key} = new $class(json_decode($value, true));
+                    }
+                } else {
+                    if($value instanceof Jsonable)
+                    {
+                        $this->{$key} = $this->{$key}->toJson();
+                    }
+                    /*elseif(is_array($value) || is_object($value))
+                    {
+                        $this->{$key} = json_encode($value);
+                    }*/
+                }
+            }
+        }
+        return $model;
+    }
+
+    /**
+     * Determines if a sequence is JSON.
+     *
+     * @param $str
+     * @return bool
+     */
+    private function isJson($str): bool
+    {
+        if(!is_string($str))
+        {
+            return false;
+        }
+        $json = json_decode($str);
+        return $json && $str != $json;
     }
 }
